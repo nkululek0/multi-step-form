9c1d828bef4dbe6805645a88c1c089776d300328
nkululek0 <nkululekovuyo0103@gmail.com>
2025-08-21 15:59:39 +0200

    (Update): removes boilerplate code and adds createReactRouter dependency

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+import { RouterProvider } from 'react-router-dom';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello React</h1>
+    </>
+  )
+};
+
+export default App;

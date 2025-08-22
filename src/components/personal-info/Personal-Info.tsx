2a98b7e3fbc2a0ff32252dd668c481e1abcdecd5
nkululek0 <nkululekovuyo0103@gmail.com>
2025-08-22 22:11:59 +0200

    (Update): personal-info - adds form and began form validation

--- src/components/personal-info/Personal-Info.tsx
+++ src/components/personal-info/Personal-Info.tsx
@@ -1,10 +1,110 @@
-import { Link } from 'react-router-dom';
+import { Button } from '../button';
+import type { PersonalInfoState, PersonalInfoAction, PersonalInfoReducer } from './Personal-Info.types';
+import { useReducer } from 'react';
+
+const reducer: PersonalInfoReducer = (state: PersonalInfoState, action: PersonalInfoAction) => {
+  const { type } = action;
+  let { name, email, phoneNumber, isValid } = state;
+
+  switch (type) {
+    case 'SET_NAME':
+      return {
+        ...state,
+        name: { value: action.payload, error: name.error }
+    };
+    case 'SET_EMAIL':
+      return {
+        ...state,
+        email: { value: action.payload, error: email.error }
+      };
+    case 'SET_PHONE_NUMBER':
+      return {
+        ...state,
+        phoneNumber: { value: action.payload, error: phoneNumber.error }
+      };
+    case 'VALIDATE':
+      const hasNameError = name.value ? true : false;
+      const hasEmailError = email.value ? true : false;
+      const hasPhoneNumberError = phoneNumber.value ? true : false;
+      isValid = hasNameError && hasEmailError && hasPhoneNumberError;
+
+      return {
+        ...state,
+        isValid: isValid
+      };
+    default:
+      return state;
+  }
+};
 
 export function PersonalInfo() {
+
+  const defaultState: PersonalInfoState = {
+    name: {
+      value: '',
+      error: '',
+    },
+    email: {
+      value: '',
+      error: '',
+    },
+    phoneNumber: {
+      value: '',
+      error: '',
+    },
+    isValid: false
+  };
+  const [personalInfo, dispatch] = useReducer(reducer, defaultState);
+  const { name, email, phoneNumber, isValid } = personalInfo;
+  const validator = () => {
+    dispatch({ type: 'VALIDATE' });
+  };
+
   return (
-    <div>
-      <h1>Personal Info</h1>
-      <button><Link to="/select-plan">Next</Link></button>
-    </div>
+    <article>
+      <header>
+        <h1>Personal Info</h1>
+        <p>Please provide your name, email address, and phone number.</p>
+      </header>
+      <form>
+        <div>
+          <div>
+            <label htmlFor='name'>*Name:</label>
+            <span></span>
+          </div>
+          <input
+            type='text'
+            required
+            value={ name.value }
+            onChange={ (event) => dispatch({ type: "SET_NAME", payload: event.target.value }) }
+          />
+        </div>
+        <div>
+          <div>
+            <label htmlFor='email'>*Email Address:</label>
+            <span></span>
+          </div>
+          <input
+            type='email'
+            required
+            value={ email.value }
+            onChange={ (event) => dispatch({ type: 'SET_EMAIL', payload: event.target.value }) }
+          />
+        </div>
+        <div>
+          <div>
+            <label htmlFor='phone'>*Phone Number:</label>
+            <span></span>
+          </div>
+          <input
+            type='tel'
+            required
+            value={ phoneNumber.value }
+            onChange={ (event) => dispatch({ type: 'SET_PHONE_NUMBER', payload: event.target.value }) }
+          />
+        </div>
+      </form>
+      <Button validationSettings={{ validate: validator, isValid: isValid, uri: '/select-plan' }}>Next</Button>
+    </article>
   );
-}
+};

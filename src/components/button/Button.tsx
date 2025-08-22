bdba3442743e9a7bd5536fbe7b74eeb4b2633bdc
nkululek0 <nkululekovuyo0103@gmail.com>
2025-08-22 22:13:46 +0200

    (Create): button - created next page button component which validates form before attempting to show next stage in form process

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,27 @@
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
+type ButtonProps = {
+  children: string;
+  validationSettings: {
+    validate: () => void
+    uri: string,
+    isValid: boolean
+  }
+};
+
+export function Button(props: ButtonProps) {
+
+  const navigate = useNavigate();
+  const { validationSettings, children } = props;
+
+  useEffect(() => {
+    if (validationSettings.isValid) {
+      navigate(validationSettings.uri);
+    }
+  }, [validationSettings.isValid]);
+
+  return (
+    <button onClick={ () => { validationSettings.validate(); } }>{ children }</button>
+  );
+};

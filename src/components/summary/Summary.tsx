0af60313bebc866005be28f2fd1f8df83305874c
nkululek0 <nkululekovuyo0103@gmail.com>
2025-09-12 16:03:54 +0200

    (Update): summary -> adds html content

--- src/components/summary/Summary.tsx
+++ src/components/summary/Summary.tsx
@@ -1,11 +1,17 @@
-import { Link } from 'react-router-dom';
+import Confirmation from '../../assets/images/icon-confirmation.svg';
 
 export function Summary() {
   return (
-    <div>
-      <h1>Finishing Up</h1>
-      <Link to='/add-ons'>Go Back</Link>
-      <button>Confirm</button>
-    </div>
+    <>
+      <article className='summary'>
+        <img src={ Confirmation } alt='confirmation image' />
+        <h1>Thank You</h1>
+        <p className="summary">
+          Thanks for confirming your subscription! We hope you have fun using
+          our platform. If you ever need support, please feel free to email us
+          at 2V6Yb@example.com
+        </p>
+      </article>
+    </>
   );
 };

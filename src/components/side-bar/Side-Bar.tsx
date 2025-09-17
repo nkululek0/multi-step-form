b1586d4c263ef0fc90dc5ec4bc10ef46d5a0c67c
nkululek0 <nkululekovuyo0103@gmail.com>
2025-09-17 18:46:34 +0200

    (Update): mobile-styling -> renames styles import variable in the side-bar component to ensure that it is consistent throughout project

--- src/components/side-bar/Side-Bar.tsx
+++ src/components/side-bar/Side-Bar.tsx
@@ -1,41 +1,41 @@
-import Styles from './Side-Bar.module.css';
+import styles from './Side-Bar.module.css';
 
 export function SideBar() {
   return (
     <>
-      <section className={ Styles['side-bar'] } >
+      <section className={ styles['side-bar'] }>
         <div className='side-bar-item'>
-          <div className={ Styles['step-indicator-wrapper']}>
+          <div className={ styles['step-indicator-wrapper'] }>
             <p>1</p>
           </div>
-          <div className={ Styles['step-indicator']}>
+          <div className={ styles['step-indicator'] }>
             <p>Step 1</p>
             <p>Your Info</p>
           </div>
         </div>
         <div className='side-bar-item'>
-          <div className={ Styles['step-indicator-wrapper']}>
+          <div className={ styles['step-indicator-wrapper'] }>
             <p>2</p>
           </div>
-          <div className={ Styles['step-indicator']}>
+          <div className={ styles['step-indicator'] }>
             <p>Step 2</p>
             <p>Select Plan</p>
           </div>
         </div>
         <div className='side-bar-item'>
-          <div className={ Styles['step-indicator-wrapper']}>
+          <div className={ styles['step-indicator-wrapper'] }>
             <p>3</p>
           </div>
-          <div className={ Styles['step-indicator']}>
+          <div className={ styles['step-indicator'] }>
             <p>Step 3</p>
             <p>Add Ons</p>
           </div>
         </div>
         <div className='side-bar-item'>
-          <div className={ Styles['step-indicator-wrapper']}>
+          <div className={ styles['step-indicator-wrapper'] }>
             <p>4</p>
           </div>
-          <div className={ Styles['step-indicator']}>
+          <div className={ styles['step-indicator'] }>
             <p>Step 4</p>
             <p>Summary</p>
           </div>

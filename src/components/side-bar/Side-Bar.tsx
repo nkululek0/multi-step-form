9725e4489e7cc301d4eff586b5d17eabdb2de6f7
nkululek0 <nkululekovuyo0103@gmail.com>
2025-09-17 16:56:23 +0200

    (Update): mobile-styling -> adds core styling for sidebar

--- src/components/side-bar/Side-Bar.tsx
+++ src/components/side-bar/Side-Bar.tsx
@@ -5,37 +5,37 @@ export function SideBar() {
     <>
       <section className={ Styles['side-bar'] } >
         <div className='side-bar-item'>
-          <div className='step-indicator-wrapper'>
+          <div className={ Styles['step-indicator-wrapper']}>
             <p>1</p>
           </div>
-          <div>
+          <div className={ Styles['step-indicator']}>
             <p>Step 1</p>
             <p>Your Info</p>
           </div>
         </div>
         <div className='side-bar-item'>
-          <div className='step-indicator-wrapper'>
+          <div className={ Styles['step-indicator-wrapper']}>
             <p>2</p>
           </div>
-          <div>
+          <div className={ Styles['step-indicator']}>
             <p>Step 2</p>
             <p>Select Plan</p>
           </div>
         </div>
         <div className='side-bar-item'>
-          <div className='step-indicator-wrapper'>
+          <div className={ Styles['step-indicator-wrapper']}>
             <p>3</p>
           </div>
-          <div>
+          <div className={ Styles['step-indicator']}>
             <p>Step 3</p>
             <p>Add Ons</p>
           </div>
         </div>
         <div className='side-bar-item'>
-          <div className='step-indicator-wrapper'>
+          <div className={ Styles['step-indicator-wrapper']}>
             <p>4</p>
           </div>
-          <div>
+          <div className={ Styles['step-indicator']}>
             <p>Step 4</p>
             <p>Summary</p>
           </div>

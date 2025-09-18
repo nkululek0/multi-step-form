0cc0059abb5775f4e4cef7e573b9490dfb02ec26
nkululek0 <nkululekovuyo0103@gmail.com>
2025-09-18 20:29:24 +0200

    (Create & Update): mobile-styling -> creates and registers core summary component styling

--- src/components/summary/Summary.tsx
+++ src/components/summary/Summary.tsx
@@ -1,16 +1,20 @@
+import styles from './Summary.module.css';
+
 import Confirmation from '../../assets/images/icon-confirmation.svg';
 
 export function Summary() {
   return (
     <>
-      <article className='summary'>
-        <img src={ Confirmation } alt='confirmation image' />
-        <h1>Thank You</h1>
-        <p className="summary">
-          Thanks for confirming your subscription! We hope you have fun using
-          our platform. If you ever need support, please feel free to email us
-          at nkuluekozikode@gmail.com
-        </p>
+      <article className={styles['summary-wrapper']}>
+        <section className={styles['summary']}>
+          <img src={ Confirmation } alt='confirmation image' />
+          <h1>Thank you!</h1>
+          <p>
+            Thanks for confirming your subscription! We hope you have fun using
+            our platform. If you ever need support, please feel free to email us
+            at nkuluekozikode@gmail.com
+          </p>
+        </section>
       </article>
     </>
   );

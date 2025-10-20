d95143cc1941c4d05b1b02e97717cebe51427a17
nkululek0 <nkululekovuyo0103@gmail.com>
2025-10-20 10:26:41 +0200

    (Update): sidebar-step-indicator-bugs -> updates sidebar-step-indicator export statement on the index.tsx file

--- src/components/side-bar/index.tsx
+++ src/components/side-bar/index.tsx
@@ -1,2 +1,2 @@
 export { SideBar } from './Side-Bar.tsx';
-export { SideBarStepIndicator } from './side-bar-step-indicator.tsx';
+export { SideBarStepIndicator } from './Side-Bar-Step-Indicator.tsx';
